aad2c10bf6d78e9e001c99edcfdce6038dbe2d58
BeastSavager <lkumarsahoo7@gmail.com>
2026-03-09 14:55:04 +0530

    Initial commit

--- src/PalindroneCheckerApp.java
+++
@@ -1,6 +0,0 @@
-public class PalindroneCheckerApp {
-    public static void main(String[] args){
-        System.out.println("Welcome to the palindrone checker app:-  ");
-        System.out.println("Modify logic for second case");
-    }
-}
